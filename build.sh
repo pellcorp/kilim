bf39b6c849ac5650482bafa44226862a684927cf
Jason Pell <jason@pellcorp.com>
2013-03-15 09:26:08 +1100

    cleaned up the maven stuff to ensure we get classes such as S03 which are generated from tests but appears to be required at runtime - go figure

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 export CLASSPATH=./classes:./testclasses:./libs/asm-all-2.2.3.jar:./libs/junit.jar:$CLASSPATH 
 
 echo making dir:  ./classes
-rm -rf ./classes
-rm -rf ./testclasses
-mkdir ./classes
-mkdir ./testclasses
+rm -rf ./target
+mkdir ./target
+mkdir ./target/classes
+mkdir ./target/testclasses
 
 echo Compiling java source ===========================================
-javac -g -d ./classes `find . -name "*.java" `
+javac -g -d ./target/classes `find . -name "*.java" `
 
 echo Compiling .j files for testing ==================================
-java -ea kilim.tools.Asm -d ./classes `find . -name "*.j"`
+java -ea kilim.tools.Asm -d ./target/classes `find . -name "*.j"`
 
 echo Weaving =========================================================
-# Weave all files under ./classes, compiling the tests to
+# Weave all files under ./targetclasses, compiling the tests to
 # ./testclasses while excluding any that match "ExInvalid". These are
 # negative tests for the Weaver.
-java -ea kilim.tools.Weaver -d ./classes -x "ExInvalid|test" ./classes
-java -ea kilim.tools.Weaver -d ./testclasses -x "ExInvalid" ./classes
+java -ea kilim.tools.Weaver -d ./target/classes -x "ExInvalid|test" ./target/classes
+java -ea kilim.tools.Weaver -d ./target/test-classes -x "ExInvalid" ./target/classes
 
 
