c2280983b861e1b1d431b4aa4798797dcfc1ea55
Jason Pell <jason@pellcorp.com>
2013-05-15 10:53:32 +1000

    updates to pom and cleanup

--- test.sh
+++
@@ -1,5 +0,0 @@
-echo "Testing Kilim Weaver"
-java -cp ./target/classes:./libs/asm-all-2.2.3.jar:./libs/junit.jar junit.textui.TestRunner kilim.test.AllNotWoven
-
-echo "Task, mailbox tests"
-java -Dkilim.Scheduler.numThreads=10 -cp ./target/test-classes:./target/classes:./libs/asm-all-2.2.3.jar:./libs/junit.jar junit.textui.TestRunner kilim.test.AllWoven
